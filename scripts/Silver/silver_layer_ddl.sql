8dcd27ee24b428c2825f77a97caf340433ede34e
Svachuta Gollavilli <sunil202pydah@gmail.com>
2025-09-01 22:41:50 -0400

    Update silver_layer_ddl.sql

--- scripts/Silver/silver_layer_ddl.sql
+++ scripts/Silver/silver_layer_ddl.sql
@@ -6,6 +6,7 @@ DDL script to create table schema in the Silver layer of the datawarehouse
 
 =============================================================================
 
+*/
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;

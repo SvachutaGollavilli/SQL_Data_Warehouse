de6bcbdffe12d6a8c0c4209f8bfc1b9caf2085d6
Svachuta Gollavilli <sunil202pydah@gmail.com>
2025-09-01 22:15:37 -0400

    Update bronze_layer_ddl.sql

--- scripts/Bronze/bronze_layer_ddl.sql
+++ scripts/Bronze/bronze_layer_ddl.sql
@@ -1,1 +1,16 @@
+/*
+
+============================================================================
+
+Create DDL script for the Bronze layer of the DataWarehouse
+
+============================================================================
+
+This script is used to create the required tables to load the data from the source systems to 
+the bronze layer of the warehouse. This is a 1:1 load without any modifications and is required to 
+integrate similar subject oriented data from various souces to a single destination before applying "Transformations"
+
+*/
+
+
 
